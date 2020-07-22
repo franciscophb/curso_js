06ea06dd6e1d786d7f76d088d84177afc3bbe05d
Francisco Araujo <franciscophb@hotmail.com>
2020-07-21 21:32:32 -0300

    add comentarios

---
+++ fundamentos/comentario.js
@@ -0,0 +1,15 @@
+// comentario de uma linha
+console.log('linha 1')
+
+/*
+Comentário de 
+múltiplas linha
+*/
+
+console.log('linha 2')
+
+/*
+*Comentário de
+*múltiplas linhas
+*/
+console.log('linha 3')
