324dc7de34f243ccac716d2f531f229a4d5b1f27
Francisco Araujo <franciscophb@hotmail.com>
2020-07-21 22:56:53 -0300

    Add numerosAlgunsCuidados.js e math.js

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0)// retorna Infinity
+console.log("10"/2)//faz a divisão normal pois ela sabe o que é
+console.log("Show!"*2)//não multiplica a frase, gerando um NaN "Not a Number"
+console.log(0.1+0.7)//não dá um resultado exato ou precisa
+//console.log(10.toString())
+console.log(10.345.toFixed(2))
